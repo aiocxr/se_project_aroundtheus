714d750854fff11f6c6ff7e3a5e43103c5dea152
AngelGaytan <a.xgel19@gmail.com>
2024-03-19 20:50:11 -0500

    finished part 1

--- scripts/index.js
+++ scripts/index.js
@@ -29,13 +29,22 @@ console.log(initialCards);
 
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
+const closeModalButton = profileEditModal.querySelector("#modal-close-button");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
 
 profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+
   profileEditModal.classList.add("modal__opened");
 });
 
-const closeModalButton = profileEditModal.querySelector("#modal-close-button");
-
 closeModalButton.addEventListener("click", () => {
+  profileDescriptionInput.value = profileDescription.textContent;
+
   profileEditModal.classList.remove("modal__opened");
 });
