855fdd898dfaad7a83345e5e55b08e9d4bec2b49
AngelGaytan <a.xgel19@gmail.com>
2024-05-01 14:05:18 -0500

    finished func with profile and add button/ some er

--- scripts/index.js
+++ scripts/index.js
@@ -45,7 +45,7 @@ const addCardForm = document.querySelector("#add-card-form");
 
 const addCardModal = document.querySelector("#add-card-modal");
 const addNewCardButton = document.querySelector(".profile__add-button");
-const closeAddButtonModal = addCardModal.querySelector("#modal-add-button");
+const closeAddModalButton = addCardModal.querySelector("#modal-add-button");
 
 // CARD ELEMENTS
 
@@ -66,53 +66,40 @@ function getCardElement(cardData) {
   return cardElement;
 }
 
-// CLOSE FUNCTIONS
+// Key Functions
 
-function closeEditPopup() {
-  profileEditModal.classList.remove("modal_opened");
+function openModal(modal) {
+  modal.classList.add("modal_opened");
 }
 
-function closeAddPopup() {
-  closeAddButtonModal.classList.remove("modal_opened");
+function closeModal(modal) {
+  modal.classList.remove("modal_opened");
 }
 
 // EVENTHANDLERS
 
-function handleProfileEditSubmit(e) {
-  e.preventDefault();
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
   profileTitle.textContent = profileTitleInput.value;
   profileDescription.textContent = profileDescriptionInput.value;
-  closeEditPopup();
-}
-
-function handleProfileAddSubmit(e) {
-  e.preventDefault();
-  profileTitle.textContent = profileTitleInput.value;
-  profileDescription.textContent = profileDescriptionInput.value;
-  closeAddPopup();
+  closeModal();
 }
 
 // EVENT LISTENERS
+profileEditForm.addEventListener("submit", handleProfileFormSubmit);
 
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
-  profileEditModal.classList.add("modal_opened");
+  openModal(profileEditModal);
 });
-addNewCardButton.addEventListener("click", () => {
-  profileTitleInput.value = profileTitle.textContent;
-  profileDescriptionInput.value = profileDescription.textContent;
-  addCardModal.classList.add("modal_opened");
-});
-closeProfileModalButton.addEventListener("click", () => {
-  closeEditPopup();
-});
-closeAddButtonModal.addEventListener("click", () => {
-  closeAddPopup();
-});
+addNewCardButton.addEventListener("click", () => openModal(addCardModal));
 
-profileEditForm.addEventListener("submit", handleProfileEditSubmit);
-addCardForm.addEventListener("submit", handleProfileAddSubmit);
+closeProfileModalButton.addEventListener("click", () =>
+  closeModal(profileEditModal)
+);
+
+closeAddModalButton.addEventListener("click", () => closeModal(addCardModal));
 
 // cards func
 
