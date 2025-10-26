08fd20540ffe42542fec0ca782280b0d75408e78
Angel Gaytan <imac3548@gmail.com>
2025-10-26 16:09:04 -0500

    working on font import paths

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 // --------------------
 // Imports
 // --------------------
+import "../vendor/fonts.css";
 import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
 import Section from "../components/Section.js";
